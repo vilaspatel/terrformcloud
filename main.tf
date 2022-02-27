0835e844e69a1ca9f0846f025d2f99501201f9ba
vilaspatel <vilaspatel.94@gmail.com>
2022-02-27 18:14:40 +0530

    emailtest

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
     name = "dcloud"
     location = "EastUS"
 }
+
+resource "azurerm_resource_group" "rg1" {
+    name = "dcloud-email"
+    location = "EastUS"
+}
