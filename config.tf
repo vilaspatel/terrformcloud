9c0b6e4eb085be9f46e705ad170a4c8d4c73e440
vilaspatel <vilaspatel.94@gmail.com>
2022-02-27 17:57:10 +0530

    all files

---
+++ config.tf
@@ -0,0 +1,20 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+}
---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg1" {
+    name = "tcloud"
+    location = "EastUS"
+}
